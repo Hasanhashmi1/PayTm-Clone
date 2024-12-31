9ee74fb6d8e19558bb6a964f7fdf00570823de1c
Hasan hashmi <88746114+Hasanhashmi1@users.noreply.github.com>
2024-12-31 06:09:41 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var coollectingdData = document.getElementById("myFormData").value;
+var btnOfSubmit = document.getElementById("subitBtn");
+
+btnOfSubmit.addEventListener("click", ()=>{
+   
+   alert("Your Data is submitted");
+});
+
+
+// function boostUp(event){
+//     event.preventDefault();
+// }
+
